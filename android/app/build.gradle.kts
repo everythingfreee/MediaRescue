87f208f139fedfad6d9eb93de59b7d515a5790fa
Sanaullah Shaheer <everythingfree36@gmail.com>
2026-09-14 12:39:45 +0430

    v1.0.8 first commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
     }
 
     signingConfigs {
